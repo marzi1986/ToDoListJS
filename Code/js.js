6b55669f2066213ba51dc7ed146c4354d4a63a07
Marzi <77510439+marzi1986@users.noreply.github.com>
2025-10-25 21:30:07 +0330

    Sorting lines

--- Code/js.js
+++ Code/js.js
@@ -5,14 +5,10 @@ const alertMessage=document.getElementById("alert-message");
 let todos= JSON.parse(localStorage.getItem("ToDos")) || [];
 const editButton=document.getElementById("edit-btn");
 const catButtons=document.querySelectorAll(".category");
-    
-
-
-
-
 const tBody=document.querySelector("tbody");
 const btnDeleteall= document.getElementById("btn-deleteAll");
 
+/////////////////////// Show Alert////////////////////
 const showAlert=(message, type)=>{
     const alert= document.createElement("p");
     alert.innerHTML="";
@@ -23,6 +19,7 @@ const showAlert=(message, type)=>{
 
     setTimeout(()=>{alert.style.display="none"}, 2000);
 }
+///////////////////// Save to local//////////////////
  const saveToLocal=()=>{
    
 
@@ -32,7 +29,7 @@ const generateId=()=>{
          const id=Math.round( Math.random() * Math.random() * Math.pow(10,8).toString());
          return id;
     }
-
+/////////////////// Display in table ////////////////
     const displayToDos=(data)=>{
 
         const listTodo= data || todos;
@@ -68,13 +65,11 @@ const generateId=()=>{
         }
 
     }
-
+////////////////////////////// Create Task ////////////////////
 const saveTasksHandler = (event)=>{
 
     const task=newTask.value;
     const date=newDate.value;
-     
-    
     const rtnID=generateId();
     const todo={rtnID , task , date , completed:false};
    
@@ -90,7 +85,7 @@ const saveTasksHandler = (event)=>{
     
     displayToDos();
 }
-
+/////////////////////////////////////// Delete All /////////
 const deleteAllHandler=(event)=>{
 
     if (todos.length){
@@ -103,44 +98,32 @@ const deleteAllHandler=(event)=>{
         showAlert("There is no task to delete");
 
 }
+/////////////////////////////////////// Delete A task /////////
 const deleteTaskHandler=(id)=>{
 
     const newTodos=todos.filter(todo => todo.rtnID !==id);
- 
     todos=newTodos;
-    console.log(todos);
     saveToLocal();
     displayToDos();
     showAlert("Task was deleted successfullt", "error");
 }
-
+/////////////////////////////////////// Toggle Do/undo ////////
 const toggleHandler=(id)=>{
-    console.log(id);
-
-    const todo= todos.find((todo)=>
-        todo.rtnID===id )
+    const todo= todos.find((todo)=> todo.rtnID===id );
     todo.completed=!todo.completed;
-    console.log(todo);
-
- saveToLocal();
+    saveToLocal();
     displayToDos();
     showAlert("status was changed successfully", "error");
-
-    
-
 }
+
+/////////////////////////////////////// Submit Edit ////////////
 const applyEdit=(event)=>{
 
     const id=parseInt(event.target.dataset.id);
- 
-    const applyTodo= todos.find((todo)=>
-        todo.rtnID === id )
-    console.log(typeof(id) , typeof(applyTodo.rtnID));
-
+    const applyTodo= todos.find((todo)=>todo.rtnID === id )
+    
    applyTodo.task=newTask.value;
    applyTodo.date=newDate.value;
-   console.log(applyTodo);
-
     saveToLocal();
     displayToDos();
     showAlert("Task was updated successfully", "success");
@@ -149,26 +132,22 @@ const applyEdit=(event)=>{
     addButton.style.display="inline-block";
     editButton.style.display="none";
 }
+
+/////////////////////////////////////// Edit Button of a task //////
 const editHandler= (id)=>{
 
     addButton.style.display="none";
     editButton.style.display="inline-block";
-
-    const editTask= todos.find(
-        (todo)=> todo.rtnID === id);
-        console.log(editTask);
-
+    const editTask= todos.find((todo)=> todo.rtnID === id);
      newTask.value=editTask.task;
      newDate.value=editTask.date;
      editButton.dataset.id=id;  
    
-    
-}
-
+ }
+/////////////////////////////////////// Filtering ////////////////
 const filterTodos=(event)=>{
 
     const filterType= event.target.dataset.filter;
-   
     let ftodos=null;
  
     switch(filterType){
@@ -186,9 +165,8 @@ const filterTodos=(event)=>{
     }
 
     displayToDos(ftodos);
-   console.log("filter", ftodos);
 }
-
+///////////////////  Event listener ///////////
 catButtons.forEach((button)=>{
     button.addEventListener("click", filterTodos);
 });
